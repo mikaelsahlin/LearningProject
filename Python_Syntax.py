9a6ce574a14feb37fc63ab71654361b2662543a9
mikaelsahlin <mikael.hg.sahlin@gmail.com>
2016-11-19 13:05:57 +0100

    Created Python_Syntax.py
    https://www.codecademy.com

---
+++ Python_Syntax.py
@@ -0,0 +1,25 @@
+""" Multiline comment start here
+var = True; # assign boolean value
+new_var = 1.234 # assign floating value
+my_int = 4; # assign itneger value
+a_string = "some text"; # assign string variable
+
+Simple Maths
+a ** b # a^b
+a%b # modulus operation rest from a in b.
+
+Special characters, use \ to escape
+for instance ' should be printed \' if used in text. 
+i.e. 'She\'s smart!'
+
+Array Indexing starts with 0.
+
+Methods
+len() - len(var)
+str() - str(var) 
+
+String methods
+lower() - var.lower()
+upper() - var.upper()
+print string_var   Prints the string variable
+""
